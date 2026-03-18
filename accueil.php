9d3a099114e86b092cffa98bffb5d59fc361bfcb
Ibrahima Sory Diallo <isorydiallo05@gmail.com>
2026-03-18 22:54:42 +0000

    fix: merge conflict

--- accueil.php
+++ accueil.php
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-</head>
-<body>
-    <h1>Hello World</h1>
-</body>
-</html>
-=======
 <?php
 require_once "entete.php";
 require_once "menu.php";
@@ -26,4 +13,3 @@ require_once "menu.php";
 <script src="static/js/articles.js"></script>
 
 <?php require_once "footer.php"; ?>
->>>>>>> 5bd35f6 (feat: add articles api)
